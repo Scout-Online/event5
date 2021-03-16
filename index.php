41cf041253aa2683b2794bd89221028955a15ec9
Scout-Online <79670760+Scout-Online@users.noreply.github.com>
2021-03-16 23:05:09 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Deac-event</title>
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+<link rel="stylesheet" href="bootstrap.min.css">
+
+<link rel="stylesheet" href="fontawesome-all.min.css">
+
+<link rel="stylesheet" href="swiper.min.css">
+
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header class="site-header">
+<div class="header-bar">
+<div class="container-fluid">
+<div class="row align-items-center">
+<div class="col-10 col-lg-4">
+<h1 class="site-branding flex">
+<a href="#">Deac-event</a>
+</h1>
+</div>
+<div class="col-2 col-lg-8">
+<nav class="site-navigation">
+<div class="hamburger-menu d-lg-none">
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+</div>
+<ul>
+
+</ul>
+</nav>
+</div>
+</div>
+</div>
+</div>
+</header>
+<div class="hero-content">
+<div class="container">
+<div class="row">
+<div class="col-12 offset-lg-2 col-lg-10">
+<div class="entry-header">
+<h2>onze stream begint over:</h2>
+
+</div>
+<div class="countdown flex flex-wrap justify-content-between" data-date="2021/04/12 17:00">
+<div class="countdown-holder">
+<div class="dday"></div>
+<label>Dagen</label>
+</div>
+<div class="countdown-holder">
+<div class="dhour"></div>
+<label>Uren</label>
+</div>
+<div class="countdown-holder">
+<div class="dmin"></div>
+<label>Minuten</label>
+</div>
+<div class="countdown-holder">
+<div class="dsec"></div>
+<label>Seconden</label>
+</div>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-12 ">
+<div class="entry-footer">
+
+<a title="Add to Calendar" class="addeventatc" data-id="Ov6181611" href="https://www.addevent.com/event/Ov6181611" target="_blank" rel="nofollow">Toevoegen aan Kalender</a> <script type="text/javascript" src="https://addevent.com/libs/atc/1.6.1/atc.min.js" async defer></script>
+
+<a href="error.html" class="btn current">Deelnemen aan livestream</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="column-2">
+    <h1>Over ons</h1>
+    <p>Super dat u interesse heeft in onze presentatie. Wij zijn 4 jongeren die afgelopen jaar in samenwerking met DEAC Teuge onderzoek hebben gedaan naar de intergratie van waterstof op GA-vliegvelden. Onze presentatie zal plaatsvinden op 12 april om 16:00 uur. Deze presentatie zal ongeveer een half uurtje duren, waarin wij proberen u zoveel mogelijk bij te leren over waterstof op vliegvelden </p>
+    <p>Graag zien we u 12 april.</p>
+    <p>Met vriendleijke groet, Bram Teunis, Maurits van der molen, Jochem Kleverwal, Mette Bouma</p>
+</div>
+<style type="text/css">
+	* {
+	  box-sizing: border-box;
+	}
+	h1 {color: black;}
+	p {color: black;}
+	/* Create two equal columns that float next to each other */
+	.column-2 {
+		
+	  float: center;
+	  width: 45%;
+	  padding: 40px;
+	  padding-left:70px;
+	  padding-right: 70px;
+	  height: 475px;
+	  margin:30px;
+	  margin-right:30px;
+	  margin-bottom: 70px;
+	  background-color: #FEDE00;
+	  line-height:2;
+	}
+</style>
+
+<script type='text/javascript' src='jquery.js'></script>
+<script type='text/javascript' src='masonry.pkgd.min.js'></script>
+<script type='text/javascript' src='jquery.collapsible.min.js'></script>
+<script type='text/javascript' src='swiper.min.js'></script>
+<script type='text/javascript' src='jquery.countdown.min.js'></script>
+<script type='text/javascript' src='circle-progress.min.js'></script>
+<script type='text/javascript' src='jquery.countTo.min.js'></script>
+<script type='text/javascript' src='custom.js'></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+</body>
+</html>
