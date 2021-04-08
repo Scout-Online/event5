f8962bafee90731cb6e324a4e8b1afaddb3c450d
Scout-Online <79670760+Scout-Online@users.noreply.github.com>
2021-04-08 12:01:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -298,9 +298,9 @@ background-color:#ffff66
 <script type='text/javascript' src='jquery.collapsible.min.js'></script>
 <script type='text/javascript' src='swiper.min.js'></script> --> 
 <script type='text/javascript' src='jquery.countdown.min.js'></script>
-<!-- <script type='text/javascript' src='circle-progress.min.js'></script>
+<!-- <script type='text/javascript' src='circle-progress.min.js'></script>-->
 <script type='text/javascript' src='jquery.countTo.min.js'></script>
-<script type='text/javascript' src='custom.js'></script> 
+<!--<script type='text/javascript' src='custom.js'></script> 
 
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>-->
 <script>
